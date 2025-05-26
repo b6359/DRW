11aed39ab65c08dbb0577e7ed8c9574baf701565
bhadresh <jb.janjmera>
2025-05-26 15:16:37 +0530

    get fitter product rate by id

---
+++ api/fitter-product-rate/byid.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once '../config/constant.php';
+include_once '../config/config.php';
+$db = new DataBase();
+
+$_REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
+$response = array();
+
+if ($_REQUEST_METHOD === 'POST') {
+     $input = json_decode(file_get_contents('php://input'), true);
+    $fitterId = isset($input['fitterId']) ? $input['fitterId'] : null;
+    $productMasterId = isset($input['productMasterId']) ? $input['productMasterId'] : null;
+    $user = $db->SELECT(TBL_FITTER_PRODUCT_RATE, null, array("fitterId" => $fitterId, "productMasterId" => $productMasterId));
+
+    if ($user == true) {
+        $total_rows = mysqli_num_rows($user);
+        $user_data = [];
+        if (intval($total_rows) > 0) {
+            while ($row = $user->fetch_assoc()) {
+                $user_data[] = array(
+                    "id" => intval($row['id']),
+                    "fitterId" => $row['fitterId'],
+                    "productMasterId" => $row['productMasterId'],
+                    "rate" => $row['rate'],
+                    "createdAt" => $row['createdAt']
+                );
+            }
+            $response["message"] = MSG_FITTER_PRODUCT_RATES_FOUND;
+            $response["status"] = 200;
+            $response["data"] = $user_data;
+
+        } else {
+            $response["message"] = MSG_FITTER_PRODUCT_RATES_NOT_FOUND;
+            $response["status"] = 404;
+            $response["data"] = null;
+        }
+    } else {
+        $response["error"] = $user;
+    }
+} else {
+    $response["message"] = MSG_METHOD_NOT_ALLOWED;
+    $response["status"] = 400;
+}
+
+echo json_encode($response);
